fa620bcd48551c62b145e2c343d3b5bbe7689fe7
Fox88x <105704459+fox88x@users.noreply.github.com>
2026-03-05 12:18:35 +0100

    Obscure DEVICE_LOGIN_NAME in thingProperties.h
    Obscured the DEVICE_LOGIN_NAME for security reasons.

--- Sala3/Sala3/thingProperties.h
+++ Sala3/Sala3/thingProperties.h
@@ -3,7 +3,7 @@
 #include <ArduinoIoTCloud.h>
 #include <Arduino_ConnectionHandler.h>
 
-const char DEVICE_LOGIN_NAME[]  = "a4cbe478-e436-4bd4-9404-2390219b83f1";
+const char DEVICE_LOGIN_NAME[]  = "................";
 
 const char SSID[]               = SECRET_SSID;    // Network SSID (name)
 const char PASS[]               = SECRET_OPTIONAL_PASS;    // Network password (use for WPA, or use as key for WEP)
